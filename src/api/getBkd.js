21af22a1857fe5f30e50c6a310c8a86ee0fff71f
nycomvv <nycomvv@MacBook-Pro-de-MVV.local>
2022-12-03 17:20:22 -0300

    Tarea Final

---
+++ src/api/getBkd.js
@@ -0,0 +1,15 @@
+async function getBkd () {
+    
+    const id = "characters"
+try {
+    const response = await axios({
+        url: process.env.REACT_APP_BKD_URL + id,
+        method: "GET",
+    })
+    return response;
+} catch (error) {
+    console.log(error);
+}
+
+}
+export default getBkd;
