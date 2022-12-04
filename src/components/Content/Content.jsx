2c8a78b68001e0c780f43b8b4445366d8a9c640a
nycomvv <nycomvv@MacBook-Pro-de-MVV.local>
2022-12-04 11:48:43 -0300

    Tarea Final

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useEffect } from 'react';
 
 export default function Content() {
+    useEffect(() => {
+        async function showData() {
+            const resp = await getBkd();
+            console.log(resp);
+            return resp;
+        }
+        showData();
+    }, []);
+
   return (
     <div>
-      ALGO
+      
     </div>
   )
 }
