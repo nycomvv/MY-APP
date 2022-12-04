2558cba9eff0da565c80a84eb15a36862ddd48d7
nycomvv <nycomvv@MacBook-Pro-de-MVV.local>
2022-12-04 10:51:10 -0300

    Tarea Final

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,7 +6,7 @@ import logoContacto from "../../assets/logoContacto.png";
 export default function Contact() {
   return (
     <div className="container-home2">
-      MArcelo Contact
+      Marcelo CONTACT
        <Content />
       <img src={logoContacto} alt="logo"></img>
       <b>Curso FrontEnd USACH 2022<br /> Marcelo Vásquez</b>
