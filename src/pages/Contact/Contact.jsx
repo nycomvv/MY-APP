d0c9a6c7da443062633a7bae1f82ef2b72e71263
nycomvv <nycomvv@MacBook-Pro-de-MVV.local>
2022-12-05 18:54:51 -0300

    Proyecto Final

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -5,6 +5,8 @@ import Content from "../../components/Content/Content"
 export default function Contact() {
   return (
     <div>
+      <div className="title">BREAKING BAD <br /></div>
+      <div><p>* REPARTO *</p></div>
        <Content />
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,16 +6,17 @@ import "../../style/Home/Home.css";
 export default function home() {
   return (
     <div>
-      <div className="title">Breaking Bad <br /> (DRAMA)</div>
+      <div className="title">BREAKING BAD</div>
+      <div><p>* Género: DRAMA * </p></div>
       <div className="text">
-          <p>Tras cumplir 50 años, Walter White (Bryan Cranston), un profesor
+          Tras cumplir 50 años, Walter White (Bryan Cranston), un profesor
           de química de un instituto de Albuquerque, Nuevo México, se entera 
           de que tiene un cáncer de pulmón incurable. Casado con Skyler (Anna Gunn)
           y con un hijo discapacitado (RJ Mitte), la brutal noticia lo impulsa a dar 
           un drástico cambio a su vida: decide, con la ayuda de un antiguo alumno
           (Aaron Paul), fabricar anfetaminas y ponerlas a la venta. Lo que pretende
           es liberar a su familia de problemas económicos cuando se produzca el fatal 
-          desenlace.</p>
+          desenlace.
       </div>
       <div className="container-center">
       Temporadas : 5<br />
